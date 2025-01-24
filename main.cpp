1d6fe99605ec9f7d93c65122f61aaf6974ac847c
p-mosquera <p.mosquera@student.pk.edu.pl>
2025-01-24 15:23:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,23 +13,17 @@ int main() {
   LCD_Initalize();
   LCD_Clear();
   LCD_GoTo(0, 0);
-  LCD_WriteText((char*)"tamagotchi ON:");
+  LCD_WriteText((char*)"tamagotchi ON ");
   _delay_ms(2000);
-
+  LCD_Clear();
 
   ADC_Init();
 
   pinMode(ledPin, OUTPUT);
   int health = 100;
   int counter = 0;
-
-  LCD_Initalize();
-  LCD_Clear();
-  LCD_GoTo(0,0);
   char buffer[10];  // Asegúrate de que el tamaño del buffer sea suficiente
-  sprintf(buffer, "Health: %d  ", health);  // Convierte el entero en una cadena
-  LCD_WriteText(buffer);  // Muestra el número en el LCD
-  _delay_ms(1000);
+
   // initialize Serial (UART) communication
   uart_init(9600,0);
   sei();
@@ -37,11 +31,12 @@ int main() {
   uint8_t data;
   uart_send_string((uint8_t*)"0.Status; 1.Feed; 2.Play \n");
   while (1) {
-    LCD_Initalize();
-    LCD_Clear();
     LCD_GoTo(0,0);
-    LCD_WriteText("Tamagotchi. ");
-    sprintf(buffer, "Health: %d \n ", health); 
+    LCD_WriteText("TAMA");
+    LCD_GoTo(1,1);
+    sprintf(buffer, "Health: %d  ", health);  // Convierte el entero en una cadena
+    LCD_WriteText(buffer);  // Muestra el número en el LCD
+    _delay_ms(1000);
     counter++;
 
     // LAB 3
@@ -51,37 +46,19 @@ int main() {
       if (data == '0') {
         // do something
         uart_send_string((uint8_t*)buffer);
-        LCD_Initalize();
-        LCD_Clear();
-        LCD_GoTo(0,0);        
-        LCD_WriteText(buffer);
       }
       // when key '1' is pressed
       else if (data == '1') {
         // do something else
         uart_send_string((uint8_t*)"Tamagotchi fed.\n");
-        LCD_Initalize();
-        LCD_Clear();
-        LCD_GoTo(0,0);
-        LCD_WriteText(buffer);
         health+=10;
-        LCD_Initalize();
-        LCD_Clear();
-        LCD_GoTo(0,0);
-        LCD_WriteText(buffer);
+
       }
       else if (data == '2') {
         //do something else
         uart_send_string((uint8_t*)"Played with Tamagotchi.\n");
-        LCD_Initalize();
-        LCD_Clear();
-        LCD_GoTo(0,0);
-        LCD_WriteText(buffer);
         health+=5;
-        LCD_Initalize();
-        LCD_Clear();
-        LCD_GoTo(0,0);
-        LCD_WriteText(buffer);
+
       }
     }
 
